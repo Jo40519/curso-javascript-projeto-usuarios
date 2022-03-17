51f2f21d5d7abd85df742cb80ce8830726e7f710
João Vitor Nunes Lobato de Souza <jaolobato85@gmail.com>
2022-03-16 22:00:49 -0300

    a

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 var nome = document.querySelector('#exampleInputName')
-var gender = document.querySelector('#form-user-create [name=gender]: checked')
+var gender = document.querySelector('#form-user-create [name=gender]:checked')
 var birth = document.querySelector('#exampleInputBirth')
 var country = document.querySelector('#exampleInputCountry')
 var email = document.querySelector('#exampleInputEmail1')
 var password = document.querySelector('#exampleInputPassword')
 var photo = document.querySelector('#exampleInputFile')
 var admin = document.querySelector('#exampleInputAdmin')
+
+
+var fields = document.querySelectorAll('#form-user-create [name]')
+
+fields.forEach(function (informacoes, index) {
+    
+    if (informacoes.name == 'gender') {
+        if (informacoes.checked) {
+            console.log('Está marcado', informacoes)
+        }
+    }
+
+})
