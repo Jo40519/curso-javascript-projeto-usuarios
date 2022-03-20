40ffba780cd7aaa8946fdf5338d154435b2b1ea3
João Vitor Nunes Lobato de Souza <jaolobato85@gmail.com>
2022-03-20 18:53:52 -0300

    a

--- index.js
+++ index.js
@@ -1,26 +1,2 @@
+let userController = new UserController('form-user-create', 'table-users');
 
-var fields = document.querySelectorAll('#form-user-create [name]')
-var user = {};
-
-function addLine() {
-    var tr = document.createElement('tr')
-    
-}
-
-document.getElementById('form-user-create').addEventListener('submit', (event) => {
-    event.preventDefault();
-        fields.forEach(function (field, index) {
-    
-    if (field.name == 'gender') {
-        if (field.checked) {
-         user[field.name] = field.value
-        }
-        
-        } else {
-            user[field.name] = field.value
-        }
-   
-
-})
-    addLine(user)
-})
