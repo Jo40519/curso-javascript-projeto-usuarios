66a8e8b660ea5391702276ebdffd852820d60e34
João Vitor Nunes Lobato de Souza <jaolobato85@gmail.com>
2022-04-25 17:58:40 -0300

    a

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 let userController = new UserController('form-user-create', 'table-users');
 
+
+let nomes = 'João Vitor Nunes Lobato de Souza'
+
+const tratarNomes = nomes.split(' ').map(nomeAtual => {
+    return nomeAtual[0].toUpperCase() + 
+    nomeAtual.toLowerCase().slice(1)
+})
+
+console.log(tratarNomes)
